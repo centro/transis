6d2b5ac76dde9d276b9d88f3b156343b668f145e
Corey Burrows <corey.burrows@centro.net>
2014-12-29 20:41:12 -0600

    Adds date parser.

--- spec/parsers_spec.js
+++ spec/parsers_spec.js
@@ -35,3 +35,72 @@ describe('parseNumber', function() {
     expect(parsers.parseNumber('1_234')).toBe(1234);
   });
 });
+
+describe('parseDate', function() {
+  it('returns null when given an invalid date string', function() {
+    expect(parsers.parseDate('foo')).toBeNull();
+    expect(parsers.parseDate('2/4/5/6')).toBeNull();
+    expect(parsers.parseDate('2')).toBeNull();
+    expect(parsers.parseDate('')).toBeNull();
+  });
+
+  it('returns null when given an impossible date', function() {
+    expect(parsers.parseDate('13/1/2014')).toBeNull();
+    expect(parsers.parseDate('8/32/2014')).toBeNull();
+    expect(parsers.parseDate('13/33/2014')).toBeNull();
+    expect(parsers.parseDate('2/29/2014')).toBeNull();
+    expect(parsers.parseDate('2014-33-13')).toBeNull();
+  });
+
+  it('returns a Date object when given a property formatted date string', function() {
+    expect(parsers.parseDate('5/3/13')).toEqual(new Date(2013, 4, 3));
+    expect(parsers.parseDate('05/03/13')).toEqual(new Date(2013, 4, 3));
+    expect(parsers.parseDate('5-3-13')).toEqual(new Date(2013, 4, 3));
+    expect(parsers.parseDate('2013-01-01')).toEqual(new Date(2013, 0, 1));
+    expect(parsers.parseDate('2013-05-03')).toEqual(new Date(2013, 4, 3));
+    expect(parsers.parseDate('2013-12-25')).toEqual(new Date(2013, 11, 25));
+  });
+
+  it('handles valid leap year dates', function() {
+    expect(parsers.parseDate('2/29/2016')).toEqual(new Date(2016, 1, 29));
+    expect(parsers.parseDate('2016-02-29')).toEqual(new Date(2016, 1, 29));
+  });
+
+  it('handles leading and trailing whitespace', function() {
+    expect(parsers.parseDate('   2/2/2013')).toEqual(new Date(2013, 1, 2));
+    expect(parsers.parseDate('2/2/2013  ')).toEqual(new Date(2013, 1, 2));
+    expect(parsers.parseDate('  2/2/2013  ')).toEqual(new Date(2013, 1, 2));
+    expect(parsers.parseDate('  2013-02-02  ')).toEqual(new Date(2013, 1, 2));
+  });
+
+  it('handles dashes as the delimiter', function() {
+    expect(parsers.parseDate('2-2-2013')).toEqual(new Date(2013, 1, 2));
+  });
+
+  it('handles 4 digit years', function() {
+    expect(parsers.parseDate('2/2/2013')).toEqual(new Date(2013, 1, 2));
+    expect(parsers.parseDate('2/2/1998')).toEqual(new Date(1998, 1, 2));
+    expect(parsers.parseDate('2/2/1000')).toEqual(new Date(1000, 1, 2));
+  });
+
+  it('treats 2 digit years between 0 and 68 as the 21st century', function() {
+    expect(parsers.parseDate('10/21/00')).toEqual(new Date(2000, 9, 21));
+    expect(parsers.parseDate('10/21/01')).toEqual(new Date(2001, 9, 21));
+    expect(parsers.parseDate('10/21/67')).toEqual(new Date(2067, 9, 21));
+    expect(parsers.parseDate('10/21/68')).toEqual(new Date(2068, 9, 21));
+  });
+
+  it('treats 2 digit years between 69 and 99 as the 20th century', function() {
+    expect(parsers.parseDate('10/21/69')).toEqual(new Date(1969, 9, 21));
+    expect(parsers.parseDate('10/21/70')).toEqual(new Date(1970, 9, 21));
+    expect(parsers.parseDate('10/21/98')).toEqual(new Date(1998, 9, 21));
+    expect(parsers.parseDate('10/21/99')).toEqual(new Date(1999, 9, 21));
+  });
+
+  it('uses the current year when a year is not given', function() {
+    var y = (new Date).getFullYear();
+    expect(parsers.parseDate('1/1')).toEqual(new Date(y, 0, 1));
+    expect(parsers.parseDate('02/02')).toEqual(new Date(y, 1, 2));
+    expect(parsers.parseDate('5/19')).toEqual(new Date(y, 4, 19));
+  });
+});
--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,50 @@
 const NUMBER_RE = /^[-]?([0-9]*[0-9][0-9]*(\.[0-9]+)?|[0]*\.[0-9]*[0-9][0-9]*)$/;
 
+// Public: Parses a string containing a number.
+//
+// s - The string to parse.
+//
+// Returns a number or `null` if parsing fails.
 export function parseNumber(s) {
   s = String(s).replace(/[^\d-.]/g, '').replace(/\.$/, '');
   if (!s.match(NUMBER_RE)) { return null; }
   return parseFloat(s, 10);
+}
+
+const MDY_DATE_RE     = /^\s*(\d{1,2})[\/-](\d{1,2})(?:[\/-](\d{2,4}))?\s*$/;
+const ISO8601_DATE_RE = /^(\d\d\d\d)-(\d\d)-(\d\d)$/;
+
+// Public: Parses a string containing a date in the following formats:
+//   - YYYY-MM-DD (ISO8601)
+//   - MM/DD
+//   - MM/DD/YY
+//   - MM/DD/YYYY
+//
+// s - The string to parse.
+//
+// Returns a `Date` or `null` if parsing fails.
+export function parseDate(s) {
+  var m, d, y, date, parts;
+
+  s = String(s).replace(/\s/g, '');
+
+  if (parts = s.match(ISO8601_DATE_RE)) {
+    y = parseInt(parts[1], 10);
+    m = parseInt(parts[2], 10) - 1;
+    d = parseInt(parts[3], 10);
+    date = new Date(y, m, d);
+    return date.getMonth() === m ? date : null;
+  }
+  else if (parts = s.match(MDY_DATE_RE)) {
+    m = parseInt(parts[1], 10) - 1;
+    d = parseInt(parts[2], 10);
+    y = parts[3] ? parseInt(parts[3], 10) : (new Date).getFullYear();
+    if (0 <= y && y <= 68) { y += 2000; }
+    if (69 <= y && y <= 99) { y += 1900; }
+    date = new Date(y, m, d);
+    return date.getMonth() === m ? date : null;
+  }
+  else {
+    return null;
+  }
 }
