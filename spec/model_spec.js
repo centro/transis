41fbf8a19bac91858da613394cad99c8cab673ed
Corey Burrows <corey.burrows@centro.net>
2015-01-01 13:53:36 -0600

    Adds Model.registerAttr and .attr.

--- spec/model_spec.js
+++ spec/model_spec.js
@@ -2,8 +2,11 @@ import "es6-shim";
 import Model from "../model";
 
 describe('Model', function () {
-  class BasicModel extends Model {
-  }
+  class BasicModel extends Model {}
+  BasicModel.attr('str', 'string');
+  BasicModel.attr('strWithDefault', 'string', {default: 'zzz'});
+  BasicModel.attr('num', 'number');
+  Model.registerModel(BasicModel);
 
   describe('.registerModel', function() {
     it('throws an exception when given something other than a Model subclass', function() {
@@ -41,6 +44,14 @@ describe('Model', function () {
     });
   });
 
+  describe('.registerAttr', function() {
+    it('throws an exception when an attribute with the given name has already been defined', function() {
+      expect(function() {
+        Model.registerAttr('string');
+      }).toThrow(new Error('Ryno.Model.registerAttr: an attribute with the name `string` has already been defined'));
+    });
+  });
+
   describe('#id=', function() {
     it('throws an exception when an attempt is made to overwrite an existing non-null value', function() {
       var m = new BasicModel({id: 1});
@@ -48,6 +59,56 @@ describe('Model', function () {
       expect(function() {
         m.id = 9;
       }).toThrow(new Error(`Ryno.Model#id=: overwriting a model's identity is not allowed: ${m}`));
+    });
+  });
+
+  describe('.attr', function() {
+    beforeEach(function() {
+      this.m = new BasicModel;
+    });
+
+    it('throws an exception if given an unregistered attr name', function() {
+      expect(function() {
+        BasicModel.attr('x', 'foo');
+      }).toThrow(new Error('Ryno.Model.attr: unknown attribute type: `foo`'));
+    });
+
+    it('defines a property on the class prototype with the given name', function() {
+      expect('str' in BasicModel.prototype).toBe(true);
+    });
+
+    it('defines a property that sets a private property based on the given name', function() {
+      expect(this.m.__str__).toBeUndefined();
+      this.m.str = 'abc';
+      expect(this.m.__str__).toBe('abc');
+    });
+
+    it('defines a property that gets a private property based on the given name', function() {
+      this.m.__str__ = 'xyz';
+      expect(this.m.str).toBe('xyz');
+    });
+
+    it('uses the `default` option as the default value when one has yet to be set', function() {
+      expect(this.m.strWithDefault).toBe('zzz');
+      this.m.strWithDefault = 'a';
+      expect(this.m.strWithDefault).toBe('a');
+      this.m.strWithDefault = undefined;
+      expect(this.m.strWithDefault).toBe('zzz');
+    });
+
+    it("returns undefined as the default value when one isn't specified", function() {
+      expect(this.m.str).toBeUndefined();
+    });
+
+    it("coerces the given value when setting via the attr's coerce method", function() {
+      this.m.num = '9';
+      expect(this.m.num).toBe(9);
+    });
+
+    it('sets the <name>BeforeCoercion property to the given value when setting', function() {
+      this.m.num = '9';
+      expect(this.m.num).toBe(9);
+      expect(this.m.numBeforeCoercion).toBe('9');
     });
   });
 });
--- src/model.js
+++ src/model.js
@@ -1,6 +1,7 @@
 import IdMap from "./id_map";
+import * as attrs from "./attrs";
 
-var classes = {};
+var classes = {}, registeredAttrs = {};
 
 const NEW      = 'new';
 const EMPTY    = 'empty';
@@ -34,10 +35,81 @@ class Model {
     return this;
   }
 
+  // Public: Returns an empty instance of the model class. An empty instance contains only an id
+  // and must be retrieved from the mapper before any of its attributes will be available. Since the
+  // model's data mapper will likely need to perform an async action to retrieve data, this method
+  // is used to construct an object that is suitable for returning from `Model.get` immediately.
+  // Once the data mapper has finished loading the actual model data, the empty model will have its
+  // attributes filled in.
+  //
+  // id - The id of the model.
+  //
+  // Returns a new instance of the class.
   static empty(id) {
     var model = new this({id: id});
     model.__sourceState__ = EMPTY;
     return model;
+  }
+
+  // Public: Registers a new attribute type available to be used on `Model` subclasses.
+  //
+  // name      - A string representing the name of the attribute type.
+  // converter - A converter object or constructor function. Converter objects must implement two
+  //             methods: `coerce` and `serialize`. The `coerce` method should take the given value
+  //             and do its best to convert it to attribute's actual type. The `serialize` method
+  //             should take the coerced value and return something suitable for JSON serialization.
+  //             If a constructor function is given here, it will be instantiated once for each
+  //             declared attribute of this type and will be passed the options object given to the
+  //             `.attr` method.
+  //
+  // Returns the receiver.
+  static registerAttr(name, converter) {
+    if (registeredAttrs[name]) {
+      throw new Error(`Ryno.Model.registerAttr: an attribute with the name \`${name}\` has already been defined`);
+    }
+
+    registeredAttrs[name] = converter;
+
+    return this;
+  }
+
+  // Public: Defines an attribute on the model class. Attributes are typed properties that can
+  // parse/coerce raw values (say from a JSON object) into objects of the property type. Calling
+  // this method will define a property on the class's `prototype` so that it is available to all
+  // instances.
+  //
+  // Attribute values are automatically coerced to their proper type when set. The original value is
+  // available in the property named "<name>BeforeCoercion".
+  //
+  // name - A string representing the name of the attribute.
+  // type - A string representing the type of the attribute (this must have been previously
+  //        registered with `Model.registerAttr`).
+  // opts - An object containing properties to pass to the attribute's converter object (default:
+  //        `{}`).
+  //   default - Used as the value of the attribute when undefined.
+  //
+  // Returns the receiver.
+  static attr(name, type, opts) {
+    var converter = registeredAttrs[type], key = `__${name}__`, def = undefined;
+
+    if (!converter) {
+      throw new Error(`Ryno.Model.attr: unknown attribute type: \`${type}\``);
+    }
+
+    if (typeof converter === 'function') { converter = new converter(opts); }
+    if (opts && ('default' in opts)) { def = opts.default; }
+
+    Object.defineProperty(this.prototype, name, {
+      get: function() {
+        return this[key] === undefined ? def : this[key];
+      },
+      set: function(v) {
+        this[key] = converter.coerce(v);
+        this[`${name}BeforeCoercion`] = v;
+      }
+    });
+
+    return this;
   }
 
   constructor(attrs) {
@@ -70,5 +142,11 @@ Model.LOADED   = LOADED;
 Model.DELETED  = DELETED;
 Model.NOTFOUND = NOTFOUND;
 
-export default Model;
+Model.registerAttr('identity', attrs.IdentityAttr);
+Model.registerAttr('string', attrs.StringAttr);
+Model.registerAttr('number', attrs.NumberAttr);
+Model.registerAttr('boolean', attrs.BooleanAttr);
+Model.registerAttr('date', attrs.DateAttr);
+Model.registerAttr('datetime', attrs.DateTimeAttr);
 
+export default Model;
