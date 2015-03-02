cb00571f1af7be993ab5464eb1833b129d7400fc
Corey Burrows <corey.burrows@centro.net>
2015-03-02 13:48:26 -0600

    Adds ReactMixin for easily updating React components when properties on a Basis object change.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import BasisObject from "./object";
 import BasisArray from "./array";
 import Model from "./model";
+import ReactMixin from "./react_mixin";
 import * as util from "./util";
 import * as parsers from "./parsers";
 import pluralize from "pluralize";
@@ -10,5 +11,6 @@ export default Object.assign({
   Array: BasisArray,
   A: BasisArray.of,
   Model,
+  ReactMixin,
   pluralize
 }, util, parsers);
